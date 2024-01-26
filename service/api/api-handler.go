7ad5e382bedf4388040aace03312affda4a9dea0
lorenzougolini <ugolini.1958654@studenti.unroma1.it>
2024-01-26 15:22:21 +0100

    corrected openapi doc and backend paths

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -14,7 +14,7 @@ func (rt *_router) Handler() http.Handler {
 	rt.router.GET("/users/:username", rt.getUserProfile)
 	rt.router.PUT("/users/:username", rt.setUsername)
 
-	rt.router.POST("/users/:username/photos/", rt.uploadNewPhoto)
+	rt.router.POST("/users/:username/photos", rt.uploadNewPhoto)
 	rt.router.DELETE("/users/:username/photos/:photoid", rt.deletePhoto)
 
 	rt.router.PUT("/users/:username/followers/:followedUsername", rt.followUser)
@@ -23,10 +23,10 @@ func (rt *_router) Handler() http.Handler {
 	rt.router.PUT("/users/:username/banned/:bannedUsername", rt.banUser)
 	rt.router.DELETE("/users/:username/banned/:unbannedUsername", rt.unbanUser)
 
-	rt.router.POST("/photos/:photoid/likes/", rt.likePhoto)
+	rt.router.POST("/photos/:photoid/likes", rt.likePhoto)
 	rt.router.DELETE("/photos/:photoid/likes/:likeid", rt.unlikePhoto)
 
-	rt.router.POST("/photos/:photoid/comments/", rt.commentPhoto)
+	rt.router.POST("/photos/:photoid/comments", rt.commentPhoto)
 	rt.router.DELETE("/photos/:photoid/comments/:commentid", rt.uncommentPhoto)
 
 	rt.router.GET("/stream", rt.getStream)
